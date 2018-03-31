01d00260f27f539083b672619c4644a738f8e380
kasajei <sevenbluesea.yk@gmail.com>
2018-03-31 15:40:32 +0900

    Customize Chat

--- App/Containers/ChatMessageScreen.js
+++ App/Containers/ChatMessageScreen.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, KeyboardAvoidingView } from 'react-native'
+import { View, ScrollView, Text, KeyboardAvoidingView, SafeAreaView } from 'react-native'
 import { connect } from 'react-redux'
-import { GiftedChat } from 'react-native-gifted-chat'
+import { GiftedChat, Bubble, Actions, Send, Time } from 'react-native-gifted-chat'
 import styles from './Styles/ChatMessageScreenStyle'
 import ChatActions from '../Redux/ChatRedux'
+import { Colors } from '../Themes';
+import { Button, Input, Icon } from 'react-native-elements'
 
 class ChatMessageScreen extends Component {
   static navigationOptions =  ({ navigation }) => {
@@ -24,17 +26,92 @@ class ChatMessageScreen extends Component {
     messages.forEach(message => this.props.messageCreate(this.props.navigation.state.params.chatId, message.text))
   }
 
-  render () {
+  renderBubble(props) {
     return (
-      <GiftedChat
-        messages={this.props.messageList}
-        onSend={messages => this.onSend(messages)}
-        user={{
-          _id: this.props.user.uid,
-          name: this.props.user.displayName,
-          avatar: this.props.user.photoURL,
+      <Bubble
+        {...props}
+        textStyle={{
+          left: {
+            color: Colors.snow,
+          },
+          right: {
+            color: Colors.snow,
+          }
+        }}
+        wrapperStyle={{
+          left: {
+            backgroundColor: Colors.fire,
+          },
+          right: {
+            backgroundColor: Colors.fire,
+          },
         }}
       />
+    );
+  }
+
+  renderCustomActions(props) {
+    return (
+      <Actions
+        {...props}
+        onPressActionButton={()=>{console.log("photo")}}
+        icon={()=><Icon name='photo' type="font-awesome" color={Colors.charcoal}/>}
+      />
+    );
+  }
+
+  renderSend(props){
+    return(
+      <Send
+      {...props}
+      alwaysShowSend={true} // bug?
+      label="Done"
+      >
+      </Send>
+    )
+  }
+
+  renderTime(props){
+    return(
+      <Time
+      {...props}
+      textStyle={{ // bug?
+        left: {
+          color: Colors.snow,
+        },
+        right: {
+          color: Colors.snow,
+        }
+      }}
+      />
+    )
+  }
+
+  render () {
+    return (
+      <View style={styles.mainContainer}>
+        <GiftedChat
+          messages={this.props.messageList}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: this.props.user.uid,
+            name: this.props.user.displayName,
+            avatar: this.props.user.photoURL,
+          }}
+          loadEarlier={true}
+          // isLoadingEarlier={true}
+          showUserAvatar={true}          
+          bottomOffset={120}
+          renderBubble={this.renderBubble}
+          renderActions={this.renderCustomActions}
+          renderSend={this.renderSend}
+          renderTime={this.renderTime}
+          // renderDay={()=>null}
+        />
+        <View style={{height:120}}>
+          <Text style={styles.subtitle}>Input below</Text>
+        </View>
+      </View>
     )
   }
 }
